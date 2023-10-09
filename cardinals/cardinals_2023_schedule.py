c596e29b328669c2e148251ecd2d76a4867a1def
Ellis Belfer <ellis@hopr.co>
2023-10-09 18:04:55 -0500

    Adding teams

---
+++ cardinals/cardinals_2023_schedule.py
@@ -0,0 +1,7 @@
+import pandas as pd
+cardinals_2023_url = 'https://en.wikipedia.org/wiki/2023_Arizona_Cardinals_season'
+
+cardinals_2023_tables = pd.read_html(cardinals_2023_url)
+cardinals_2023_schedule = cardinals_2023_tables[8]
+print(cardinals_2023_schedule)
+cardinals_2023_schedule.to_csv("cardinalsschedule2023.csv")
---
+++ eagles_2023_schedule.py
@@ -0,0 +1,7 @@
+import pandas as pd
+eagles_2023_url = 'https://en.wikipedia.org/wiki/2023_Philadelphia_Eagles_season'
+
+eagles_2023_tables = pd.read_html(eagles_2023_url)
+eagles_2023_schedule = eagles_2023_tables[9]
+print(eagles_2023_schedule)
+eagles_2023_schedule.to_csv("eaglesschedule2023.csv")
