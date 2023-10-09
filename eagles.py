1656b98c9e6f9d9e717dee6971f4dc7e7c561df1
Ellis Belfer <ellis@hopr.co>
2023-10-09 11:02:35 -0500

    Used pandas instead of beautiful soup

--- eagles.py
+++ eagles.py
@@ -1,17 +1,7 @@
-from bs4 import BeautifulSoup
-import requests
 import pandas as pd
-import io
-
-
+import numpy as np
 url = 'https://en.wikipedia.org/wiki/2023_Philadelphia_Eagles_season'
-source = requests.get(url).text
-
-soup = BeautifulSoup (source, 'lxml')
 
 
-
-tables = soup.find_all('table', class_ ='wikitable')
-for x in tables:
-  df = pd.read_html(str(x))  
-  print (df)
+schedule =pd.read_html(url)
+print (schedule[9])
