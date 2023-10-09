91163d89d099c2776828da3cf3141f08d8287e2d
Ellis Belfer <ellis@hopr.co>
2023-10-09 11:23:27 -0500

    Saved output as csv file using .to_csv

--- eagles.py
+++ eagles.py
@@ -3,5 +3,7 @@ import numpy as np
 url = 'https://en.wikipedia.org/wiki/2023_Philadelphia_Eagles_season'
 
 
-schedule =pd.read_html(url)
-print (schedule[9])
+tables = pd.read_html(url)
+schedule = tables[9]
+print(schedule)
+schedule.to_csv("eaglesschedule2023.csv")
